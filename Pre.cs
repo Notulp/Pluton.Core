93d1c8609f53b6e325d960fe6b2221c168e4a287
Unknown <balugyanszky.gabor@gmail.com>
2016-10-25 06:10:01 +0100

    preevent #1

---
+++ Pre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pluton.Core
+{
+	public class Pre<T> : CountedInstance where T : Event
+	{
+		public T Event;
+
+		public bool IsCanceled = false;
+
+		public Pre(params object[] args)
+		{
+			Event = Activator.CreateInstance(typeof(T), args) as T;
+		}
+
+		public void Cancel()
+		{
+			IsCanceled = true;
+		}
+	}
+}
+
