c0c35133fad0fe152bd7b8034d07acf1c3554a8b
Snak <urimr26cat@gmail.com>
2016-10-25 18:15:23 +0200

    Adding pre event cancel reason just in case some event needs it

--- Pre.cs
+++ Pre.cs
@@ -1,22 +1,28 @@
-﻿using System;
+using System;
 
 namespace Pluton.Core
 {
-	public class Pre<T> : CountedInstance where T : Event
-	{
-		public T Event;
+    public class Pre<T> : CountedInstance where T : Event
+    {
+        public T Event;
 
-		public bool IsCanceled = false;
+        public bool IsCanceled = false;
+        public string Reason = "";
 
-		public Pre(params object[] args)
-		{
-			Event = Activator.CreateInstance(typeof(T), args) as T;
-		}
+        public Pre(params object[] args)
+        {
+            Event = Activator.CreateInstance(typeof(T), args) as T;
+        }
 
-		public void Cancel()
-		{
-			IsCanceled = true;
-		}
-	}
+        public void Cancel()
+        {
+            IsCanceled = true;
+        }
+
+        public void Cancel(string reason)
+        {
+            IsCanceled = true;
+            Reason = reason;
+        }
+    }
 }
-
